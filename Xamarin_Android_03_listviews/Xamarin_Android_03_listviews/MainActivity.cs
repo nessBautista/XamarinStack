d38962445d0fb0b752d23d51a4d55b015470218d
Nestor Javier Hernandez Bautista <nestor.hernandez@definityfirst.com>
2017-09-10 23:28:23 -0500

    Transfer knowledge from Android to Xamarin

---
+++ Xamarin_Android_03_listviews/Xamarin_Android_03_listviews/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content.Res;
+using Android.Content;
+
+
+namespace Xamarin_Android_03_listviews
+{
+    [Activity(Label = "Xamarin_Android_03_listviews", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        protected ListView ListView;		
+		string[] items = Application.Context.Resources.GetStringArray(Resource.Array.clothing);
+ 
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, items);
+            ListView = (Android.Widget.ListView) FindViewById(Resource.Id.listView);
+            ListView.Adapter = adapter;
+        }
+    }
+}
+
---
+++ Xamarin_Android_03_listviews/Xamarin_Android_03_listviews/ProductListAdapter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Xamarin_Android_03_listviews
+{
+    public class ProductListAdapter
+    {
+        public ProductListAdapter()
+        {
+        }
+    }
+}
