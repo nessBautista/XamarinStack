6481103dbcf2317042de65e9090e5c8436f713d9
Nestor Javier Hernandez Bautista <nestor.hernandez@definityfirst.com>
2017-10-07 12:37:27 -0500

    listviews

--- Xamarin_Android_03_listviews/Xamarin_Android_03_listviews/MainActivity.cs
+++ Xamarin_Android_03_listviews/Xamarin_Android_03_listviews/MainActivity.cs
@@ -11,14 +11,16 @@ namespace Xamarin_Android_03_listviews
     public class MainActivity : Activity
     {
         protected ListView ListView;		
-		string[] items = Application.Context.Resources.GetStringArray(Resource.Array.clothing);
- 
+		
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            //Simple item lists
+            string[] items = Application.Context.Resources.GetStringArray(Resource.Array.clothing);
             ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, items);
             ListView = (Android.Widget.ListView) FindViewById(Resource.Id.listView);
             ListView.Adapter = adapter;
